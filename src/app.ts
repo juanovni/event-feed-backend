8ff38e5569ba968babc4962676abe3820ad5fb89
vista-juan-constantine <juan.constantine@vistadash.com>
2025-10-22 16:35:04 -0500

    install cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import cors from "cors";
 import "express-async-errors";
+
 import userRoutes from "./routes/user/user.routes";
 import eventRoutes from "./routes/event/event.routes";
-
 /* import { errorHandler } from "./middlewares/errorHandler"; */
 
 const app = express();
+
+// ✅ Configurar CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000", // Frontend local (Next.js)
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true, // Si usas cookies o headers personalizados
+  })
+);
+
 
 app.use(express.json());
 
