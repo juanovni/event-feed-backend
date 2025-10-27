e4aeaa49989555d31c3819bb81f17ce392158513
vista-juan-constantine <juan.constantine@vistadash.com>
2025-10-27 10:11:52 -0500

    Removed console.log

--- src/controllers/event/event.controller.ts
+++ src/controllers/event/event.controller.ts
@@ -4,8 +4,6 @@ import * as eventService from "../../services/event/event.service";
 export const listEventsHandler = async (req: Request, res: Response) => {
   const userId = req.user?.id;
 
-  console.log(userId)
-
   const isFollowing = req.query.isFollowing as boolean | undefined;
 
   const events = await eventService.getEvents(String(userId), isFollowing);
